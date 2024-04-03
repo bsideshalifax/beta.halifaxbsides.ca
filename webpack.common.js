355793da7d85cf25e0b147658e52d704ffdca5ff
Jeffrey Hann <1507841+obihann@users.noreply.github.com>
2024-04-03 09:28:06 -0300

    updating cod and a few small typos

--- webpack.common.js
+++ webpack.common.js
@@ -10,6 +10,12 @@ module.exports = {
    },
 
    plugins: [
+      new CopyPlugin({
+         patterns: [
+           { from: "dist/assets", to: "assets" }
+         ],
+       }),
+      // index.html
       new HtmlWebpackPlugin({
          inject: true,
          filename: 'index.html',
@@ -29,11 +35,6 @@ module.exports = {
             'viewport': "width=device-width, initial-scale=1, shrink-to-fit=no",
          }
       }),
-      new CopyPlugin({
-         patterns: [
-           { from: "dist/assets", to: "assets" }
-         ],
-       }),
       new FaviconsWebpackPlugin({
          logo: 'dist/assets/favicons/favicon.png',
          inject: true,
@@ -48,6 +49,55 @@ module.exports = {
         path: 'src/partials/home.html',
         location: 'body',
         priority: 'high'
+      }),
+      new HtmlWebpackPartialsPlugin({
+        path: 'src/partials/footer.html',
+        location: 'body',
+        priority: 'low'
+      }),
+      // cod.html
+      new HtmlWebpackPlugin({
+         inject: true,
+         filename: 'cod.html',
+         template: 'dist/index.html',
+         meta: {
+            'description': { name: 'description', contnet: 'BSides is a community-driven framework for building events for and by information security community members.' },
+            'keyword': { name: 'keywords', content: 'Event, BSides, Halifax, Cyber, Security, infosec' },
+            'og:title': { property: 'og:title', content: 'BSides Halifax' },
+            'og:description': { property: 'og:description', content: 'BSides is a community-driven framework for building events for and by information security community members.' },
+            'og:type': { property: 'og:type', content: 'website' },
+            'og:url': { property: 'og:url', content: 'https://halifaxbsides.ca/' },
+            'og:image': { property: 'og:image', content: 'https://halifaxbsides.ca/assets/seo/sharing-facebook.png' },
+            'twitter:card': { name: 'twitter:card', content: 'summary_large_image' },
+            'twitter:title': { name: 'twitter:title', content: 'BSides Halifax' },
+            'twitter:description': { name: 'twitter:description', content: 'BSides is a community-driven framework for building events for and by information security community members.' },
+            'twitter:image': { name: 'twitter:image', content: 'https://halifaxbsides.ca/assets/seo/sharing-twitter.png' },
+            'viewport': "width=device-width, initial-scale=1, shrink-to-fit=no",
+         }
+      }),
+      new FaviconsWebpackPlugin({
+         logo: 'dist/assets/favicons/favicon.png',
+         template_filename: 'cod.html',
+         inject: true,
+         prefix: 'assets/favicons/',
+      }),
+      new HtmlWebpackPartialsPlugin({
+        path: 'src/partials/analytics.html',
+        template_filename: 'cod.html',
+        location: 'head',
+        priority: 'high'
+      }),
+      new HtmlWebpackPartialsPlugin({
+        path: 'src/partials/cod.html',
+        template_filename: 'cod.html',
+        location: 'body',
+        priority: 'high'
+      }),
+      new HtmlWebpackPartialsPlugin({
+        path: 'src/partials/footer.html',
+        template_filename: 'cod.html',
+        location: 'body',
+        priority: 'low'
       }),
    ],
 
